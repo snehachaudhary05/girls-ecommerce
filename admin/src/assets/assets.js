638efa60fb8b801228a0a9e1399c8a0902d76f70
Sneha Chaudhary <chaudharysneha693@gmail.com>
2026-01-27 23:46:39 +0530

    Fix admin logo import filename case

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -1,4 +1,4 @@
-import logo from './GLOWADMIN.PNG'
+import logo from './GLOWADMIN.png'
 import add_icon from './add_icon.png'
 import order_icon from './order_icon.png'
 import upload_area from './upload_area.png'
